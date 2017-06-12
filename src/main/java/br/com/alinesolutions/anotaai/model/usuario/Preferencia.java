378522c5448f271f3d77306e86e6712fa504927c
Gleidson Guimarães Moura <gleidson.gmoura@gmail.com>
2017-06-12 13:52:45 -0300

    i18n

--- src/main/java/br/com/alinesolutions/anotaai/model/usuario/Preferencia.java
+++ src/main/java/br/com/alinesolutions/anotaai/model/usuario/Preferencia.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 
 import com.sun.istack.NotNull;
 
+import br.com.alinesolutions.anotaai.i18n.Locale;
 import br.com.alinesolutions.anotaai.model.BaseEntity;
 import br.com.alinesolutions.anotaai.model.produto.Produto;
 
@@ -11,9 +12,12 @@ import br.com.alinesolutions.anotaai.model.produto.Produto;
 public class Preferencia extends BaseEntity<Long, Produto> {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@NotNull
 	private Integer itensPerPage;
+
+	@NotNull
+	private Locale locale;
 
 	public Integer getItensPerPage() {
 		return itensPerPage;
@@ -21,6 +25,14 @@ public class Preferencia extends BaseEntity<Long, Produto> {
 
 	public void setItensPerPage(Integer itensPerPage) {
 		this.itensPerPage = itensPerPage;
+	}
+
+	public Locale getLocale() {
+		return locale;
+	}
+
+	public void setLocale(Locale locale) {
+		this.locale = locale;
 	}
 
 }
