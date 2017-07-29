0ede94d3c5d19348b6ea0fb0b605462f7e6b325d
Gleidson <gleidson.gmoura@gmail.com>
2017-07-29 17:34:24 -0300

    configuracao caderneta

--- src/main/java/br/com/alinesolutions/anotaai/model/venda/ConfiguracaoCaderneta.java
+++ src/main/java/br/com/alinesolutions/anotaai/model/venda/ConfiguracaoCaderneta.java
@@ -1,5 +1,7 @@
 package br.com.alinesolutions.anotaai.model.venda;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 
 import org.hibernate.annotations.SQLDelete;
@@ -11,12 +13,27 @@ import br.com.alinesolutions.anotaai.model.BaseEntity;
 @Where(clause = "ativo = true")
 @SQLDelete(sql = "update ConfiguracaoCaderneta set ativo = false where id = ?")
 public class ConfiguracaoCaderneta extends BaseEntity<Long, ConfiguracaoCaderneta> {
-	
+
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	private Integer qtdDiasDuracaoFolha;
-	
-	
+
+	private Date dataInicioAnotacao;
+
+	public Integer getQtdDiasDuracaoFolha() {
+		return qtdDiasDuracaoFolha;
+	}
+
+	public void setQtdDiasDuracaoFolha(Integer qtdDiasDuracaoFolha) {
+		this.qtdDiasDuracaoFolha = qtdDiasDuracaoFolha;
+	}
+
+	public Date getDataInicioAnotacao() {
+		return dataInicioAnotacao;
+	}
+
+	public void setDataInicioAnotacao(Date dataInicioAnotacao) {
+		this.dataInicioAnotacao = dataInicioAnotacao;
+	}
 
 }
