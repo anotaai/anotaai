9ae5ac3c2277f3c0505a2418172913b3c493a3d2
Gleidson <gleidson.gmoura@gmail.com>
2017-09-22 01:08:57 -0300

    regra folha caderneta

--- src/main/java/br/com/alinesolutions/anotaai/service/app/FolhaCadernetaService.java
+++ src/main/java/br/com/alinesolutions/anotaai/service/app/FolhaCadernetaService.java
@@ -1,11 +1,7 @@
 package br.com.alinesolutions.anotaai.service.app;
 
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
-import java.util.Calendar;
 import java.util.Date;
-import static java.time.temporal.TemporalAdjusters.*;
 
 import javax.annotation.Resource;
 import javax.ejb.EJB;
@@ -62,58 +58,33 @@ public class FolhaCadernetaService {
 			caderneta = em.getReference(Caderneta.class, caderneta.getId());
 			folha.setCaderneta(caderneta);
 			folha.setDataCriacao(new Date());
-			
-			LocalDate dataAbertura = caderneta.getDataAbertura().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-			Integer diaAbertura = dataAbertura.getDayOfMonth();
-			Integer ultimoDiaMesDataAbertura = dataAbertura.with(lastDayOfMonth()).getDayOfMonth();
 			Integer diaBase = caderneta.getConfiguracao().getDiaBase();
-			
-			LocalDate now = LocalDate.now();
-			Long diasCaderneta = ChronoUnit.DAYS.between(dataAbertura, now);
 			Integer qtdDiasDuracaoFolha = caderneta.getConfiguracao().getQtdDiasDuracaoFolha();
-			
-			Calendar dataInicial = Calendar.getInstance();
-			Calendar dataFinal = Calendar.getInstance();
-			Integer qtdDiasPeriodoInicial = diaBase >= diaAbertura ? diaBase : ultimoDiaMesDataAbertura - diaAbertura + diaBase;
-			if (diasCaderneta / qtdDiasDuracaoFolha > 0) {
-				Integer equalizador = calcularEqualizador(dataAbertura, qtdDiasPeriodoInicial);
-				dataInicial.add(Calendar.DAY_OF_MONTH, (int) ((diasCaderneta - qtdDiasPeriodoInicial - equalizador) % qtdDiasDuracaoFolha * -1));
-				dataFinal.setTimeInMillis(dataInicial.getTimeInMillis());
-			} else {
-				dataInicial.setTime(caderneta.getDataAbertura());
-				dataFinal.setTimeInMillis(dataInicial.getTimeInMillis());
-			}
-			dataFinal.add(Calendar.DAY_OF_MONTH, qtdDiasDuracaoFolha);
-			System.out.println(dataInicial.getTime());
-			System.out.println(dataFinal.getTime());
+			LocalDate dataInicial = calcularDataInicioFolhaCaderneta(diaBase, qtdDiasDuracaoFolha);
+			LocalDate dataFinal = dataInicial.plusDays(qtdDiasDuracaoFolha);
+			System.out.println(dataInicial);
+			System.out.println(dataFinal);
 		}
 		return folha;
 	}
-
-
-	private Integer calcularEqualizador(LocalDate dataAbertura, Integer qtdDiasPeriodoInicial) {
-		dataAbertura = dataAbertura.plusDays(qtdDiasPeriodoInicial);
-		LocalDate primeiroDiaMesAtual  = LocalDate.now().with(firstDayOfMonth());
-		Integer count = 0;
-		while (dataAbertura.isBefore(primeiroDiaMesAtual)) {
-			switch (dataAbertura.with(lastDayOfMonth()).getDayOfMonth()) {
-			case 28:
-				count -= 2;
-				break;
-			case 29:				
-				count -= 1;
-				break;
-			case 31:
-				count++;
-				break;
-			default:
-				break;
-			}
-			dataAbertura = dataAbertura.with(firstDayOfNextMonth());
-		}
-		System.out.println(count);
-		return count;
-	}
 	
-
+	private LocalDate calcularDataInicioFolhaCaderneta(Integer diaBase, Integer qtdDiasDuracaoFolha) {
+		LocalDate now = LocalDate.now();
+		Integer countDay = 0;
+		//quantidade de dias entre o dia base e a data atual
+		while (!diaBase.equals(now.getDayOfMonth())) {
+			countDay++;
+			now = now.plusDays(-1);
+		}
+		LocalDate dataBase = LocalDate.now().plusDays(countDay * -1);
+		LocalDate diaInicioFolhaCaderneta = dataBase;
+		now = LocalDate.now();
+		while (dataBase.isBefore(now)) {
+			dataBase = dataBase.plusDays(qtdDiasDuracaoFolha);
+			if (dataBase.isBefore(now)) {
+				diaInicioFolhaCaderneta = dataBase;				
+			}
+		}
+		return diaInicioFolhaCaderneta;
+	}
 }
