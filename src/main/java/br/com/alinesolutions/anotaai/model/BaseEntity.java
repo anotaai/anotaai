9f4ae7ed3d586569d0aedb2c98fa82865e86968d
Gleidson <gleidson.gmoura@gmail.com>
2017-10-17 15:04:09 -0200

    ignorar ativo ao serializar objeto

--- src/main/java/br/com/alinesolutions/anotaai/model/BaseEntity.java
+++ src/main/java/br/com/alinesolutions/anotaai/model/BaseEntity.java
@@ -13,6 +13,7 @@ import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -31,6 +32,7 @@ public abstract class BaseEntity<ID, T extends BaseEntity<?, ?>> implements Seri
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private ID id;
 
+	@JsonIgnore
 	private Boolean ativo;
 
 	public ID getId() {
