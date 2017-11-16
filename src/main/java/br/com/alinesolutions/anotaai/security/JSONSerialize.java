4da1ccd22f279e6e5453d8ce48efa941db1e64e6
Gleidson <gleidson.gmoura@gmail.com>
2017-11-16 11:09:37 -0200

    serializador padrao

--- src/main/java/br/com/alinesolutions/anotaai/security/JSONSerialize.java
+++ src/main/java/br/com/alinesolutions/anotaai/security/JSONSerialize.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.util.Optional;
 
 import javax.annotation.Priority;
 import javax.ws.rs.Priorities;
@@ -15,6 +16,8 @@ import org.jboss.resteasy.plugins.providers.jackson.ResteasyJackson2Provider;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @Provider
 @Priority(Priorities.ENTITY_CODER)
@@ -24,10 +27,17 @@ public class JSONSerialize extends ResteasyJackson2Provider {
 	public void writeTo(Object value, Class<?> type, Type genericType, Annotation[] annotations, MediaType json, MultivaluedMap<String, Object> headers,
 			OutputStream body) throws IOException {
 
-		ObjectMapper mapper = locateMapper(type, json);
-		mapper.enable(SerializationFeature.INDENT_OUTPUT);
-
 		super.writeTo(value, type, genericType, annotations, json, headers, body);
+	}
+
+	@Override
+	public ObjectMapper locateMapper(Class<?> type, MediaType mediaType) {
+
+		FilterProvider filters = new SimpleFilterProvider().setFailOnUnknownId(false);
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+		mapper.writer(filters);
+		return Optional.ofNullable(_mapperConfig.getConfiguredMapper()).orElse(mapper);
 	}
 
 }
