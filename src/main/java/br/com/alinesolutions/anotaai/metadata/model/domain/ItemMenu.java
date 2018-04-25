beb73650899859a43537bfc3223594e44e1bfec7
Gleidson <gleidson.gmoura@gmail.com>
2018-04-25 00:21:17 -0300

    Internacionalizacao do menu

--- src/main/java/br/com/alinesolutions/anotaai/metadata/model/domain/ItemMenu.java
+++ src/main/java/br/com/alinesolutions/anotaai/metadata/model/domain/ItemMenu.java
@@ -9,36 +9,34 @@ import com.fasterxml.jackson.databind.node.JsonNodeType;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ItemMenu {
 
-	CADASTRO_CONSUMIDOR("Consumidor", Menu.PRINCIPAL, Icon.GLYPHICON_USER, Icon.USER, "/consumidor", Perfil.CLIENTE),
-	GRUPO_PRODUTO("Grupo Produto", Menu.PRINCIPAL, Icon.GLYPHICON_TH, Icon.TH, "/grupoproduto", Perfil.CLIENTE),
-	SETOR("Setor", Menu.PRINCIPAL, Icon.GLYPHICON_LIST, Icon.TH_LIST, "/setor", Perfil.CLIENTE),
-	PRODUTO("Produto", Menu.PRINCIPAL, Icon.GLYPHICON_BARCODE, Icon.BARCODE, "/produto", Perfil.CLIENTE),
-	CONFIGURACAO_CADERNETA("Configuracao Caderneta", Menu.PRINCIPAL, Icon.GLYPHICON_EDIT, Icon.SETTINGS_APPLICATIONS, "/configuracaocaderneta", Perfil.CLIENTE),
-	CADERNETA("Caderneta", Menu.PRINCIPAL, Icon.GLYPHICON_BOOK, Icon.BOOK, "/caderneta", Perfil.CLIENTE),
+	CADASTRO_CONSUMIDOR("menu.principal.consumidor", Menu.PRINCIPAL, Icon.USER, "/consumidor", Perfil.CLIENTE),
+	GRUPO_PRODUTO("menu.principal.grupoproduto", Menu.PRINCIPAL, Icon.TH, "/grupoproduto", Perfil.CLIENTE),
+	SETOR("menu.principal.setor", Menu.PRINCIPAL, Icon.TH_LIST, "/setor", Perfil.CLIENTE),
+	PRODUTO("menu.principal.produto", Menu.PRINCIPAL, Icon.BARCODE, "/produto", Perfil.CLIENTE),
+	CONFIGURACAO_CADERNETA("menu.principal.configuracaocaderneta", Menu.PRINCIPAL, Icon.SETTINGS_APPLICATIONS, "/configuracaocaderneta", Perfil.CLIENTE),
+	CADERNETA("menu.principal.caderneta", Menu.PRINCIPAL, Icon.BOOK, "/caderneta", Perfil.CLIENTE),
 	//FOLHA("Folha", "folha", Menu.PRINCIPAL, Perfil.CONSUMIDOR, Icon.GLYPHICON_SEARCH),
-	VENDA("Venda", Menu.PRINCIPAL, Icon.GLYPHICON_PENCIL, Icon.PENCIL, "/venda", Perfil.CLIENTE),
+	VENDA("menu.principal.venda", Menu.PRINCIPAL, Icon.PENCIL, "/venda", Perfil.CLIENTE),
 	//Avaliar fase 2 / adequar o modelo
 	//PEDIDO("Pedido", "app.pedido", Menu.PRINCIPAL, Icon.GLYPHICON_FILE, Perfil.CONSUMIDOR, Icon.FILE, "/pedido"),
-	ANOTA_AI_HOME("Anota ai", Menu.SUPERIOR, Icon.GLYPHICON_FIRE, null, null, Perfil.CLIENTE),
-	PERFIL("Perfil", Menu.SUPERIOR, Icon.GLYPHICON_USER, null, null, Perfil.CLIENTE),
-	ENTRADA_MERCADORIA("Entrada de Mercadoria", Menu.PRINCIPAL, Icon.GLYPHICON_ARROW_LEFT, Icon.ARROW_LEFT, "/entradamercadoria", Perfil.CLIENTE);
+	ANOTA_AI_HOME("menu.principal.home", Menu.SUPERIOR, null, "/home", Perfil.CLIENTE),
+	PERFIL("menu.principal.perfil", Menu.SUPERIOR, null, "/perfil", Perfil.CLIENTE),
+	ENTRADA_MERCADORIA("menu.principal.entradamercadoria", Menu.PRINCIPAL, Icon.ARROW_LEFT, "/entradamercadoria", Perfil.CLIENTE);
 	
-	private ItemMenu(String descricao, Menu menu, Icon icon, Icon iconeMaterial, String url, Perfil... perfis) {
-		this.descricao = descricao;
+	private ItemMenu(String key, Menu menu, Icon icon, String url, Perfil... perfis) {
+		this.key = key;
 		this.menu = menu;
 		this.icon = icon;
 		this.perfis = perfis;
 		this.url = url;
-		this.iconeMaterial = iconeMaterial;
 	}
 
-	private String descricao;
+	private String key;
 	private Menu menu;
 	@JsonIgnore
 	private Perfil[] perfis;
 	private String action;
 	private Icon icon;
-	private Icon iconeMaterial;
 	private String url;
 	
 	public String getUrl() {
@@ -48,19 +46,11 @@ public enum ItemMenu {
 	public void setUrl(String url) {
 		this.url = url;
 	}
-	
-	public Icon getIconeMaterial() {
-		return iconeMaterial;
-	}
-	
-	public void setIconeMaterial(Icon iconeMaterial) {
-		this.iconeMaterial = iconeMaterial;
+
+	public String getKey() {
+		return key;
 	}
 
-	public String getDescricao() {
-		return descricao;
-	}
-	
 	public Menu getMenu() {
 		return menu;
 	}
@@ -76,7 +66,7 @@ public enum ItemMenu {
 	public Icon getIcone() {
 		return icon;
 	}
-	
+
 	// TODO - Adicionar metodos dinamicamente
 	public String getType() {
 		return this.toString();
