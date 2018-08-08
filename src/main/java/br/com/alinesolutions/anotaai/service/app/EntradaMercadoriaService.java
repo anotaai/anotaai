219a57f4d8638c95acdacc93d42f635ec10461e5
Gleidson <gleidson.gmoura@gmail.com>
2018-08-07 23:06:37 -0300

    incluir dada de cadastro da entrada de mercadoria

--- src/main/java/br/com/alinesolutions/anotaai/service/app/EntradaMercadoriaService.java
+++ src/main/java/br/com/alinesolutions/anotaai/service/app/EntradaMercadoriaService.java
@@ -150,6 +150,7 @@ public class EntradaMercadoriaService {
 	}
 
 	public void updateItemEntrada(EntradaMercadoria entradaMercadoria) {
+		entradaMercadoria.setDataCadastro(ZonedDateTime.now());
 		entradaMercadoria.getItens().stream().forEach(e -> {
 			e.setEntradaMercadoria(entradaMercadoria);
 		});
